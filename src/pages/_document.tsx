611593bebd7d8063773384bd8c7307bdaccf55a8
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-03-23 03:04:11 -0300

    fix: globals styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>dev.finance$</title>
-
-        <link rel="stylesheet" href="/style.css" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/assets/logo.svg" />
       </Head>
       <body>
         <Main />
